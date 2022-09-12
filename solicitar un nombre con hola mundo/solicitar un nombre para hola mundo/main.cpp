6022cc4197161af9b34c994653c20505d6cf46ac
Aniee15 <80296187+Aniee15@users.noreply.github.com>
2022-09-12 14:38:15 -0600

    tarea1
    solicitar un nombre para hola mundo

---
+++ solicitar un nombre con hola mundo/solicitar un nombre para hola mundo/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   string nombre;
+    cout << " Ingresa tu nombre aqui " << endl;
+    cin>>nombre;
+    cout<<"Hola "<<nombre<<endl;
+    return 0;
+}
