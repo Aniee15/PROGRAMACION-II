4caad25f61dbed6649594827de644c96e745c397
Aniee15 <80296187+Aniee15@users.noreply.github.com>
2022-09-12 14:34:52 -0600

    tarea1
    hola mundo
    hola mundo con nombre
    cuadrado de un numero
    suma de dos numeros

---
+++ cuadrado de un numero/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+    cout << "Dame un numero" << endl;
+    cin>>numero;
+    cout<<"El cuadrado del numero "<<numero<<" es : "<<numero*numero << endl;
+    return 0;
+}
---
+++ suma de dos numeros/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+     double numero1,numero2;
+    cout << "Dame el primer valor" << endl;
+    cin>>numero1;
+      cout << "Dame el segundo valor" << endl;
+    cin>>numero2;
+
+    cout<<" La suma de los dos numeros es: "<<numero1+numero2 << endl;
+    return 0;
+}
